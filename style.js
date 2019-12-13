21fbb35cdc8a9e62f758c4993357688ca9cf443d
佐藤周平 <satou@dot1x0573.14-192-19.wlan.ms.dendai.ac.jp>
2019-12-13 14:07:37 +0900

    スクロール幅の修正

--- style.js
+++ style.js
@@ -1,14 +1,14 @@
 $('#item1Scroll').click(function(){
-    $("html,body").animate({ scrollTop: $('#content1').offset().top - 200 });
+    $("html,body").animate({ scrollTop: $('#content1').offset().top});
 });
 $('#item2Scroll').click(function () {
-    $("html,body").animate({ scrollTop: $('#content2').offset().top - 200 });
+    $("html,body").animate({ scrollTop: $('#content2').offset().top});
 });
 $('#item3Scroll').click(function () {
-    $("html,body").animate({ scrollTop: $('#content3').offset().top - 200 });
+    $("html,body").animate({ scrollTop: $('#content3').offset().top});
 });
 $('#item4Scroll').click(function () {
-    $("html,body").animate({ scrollTop: $('#content4').offset().top - 200 });
+    $("html,body").animate({ scrollTop: $('#content4').offset().top});
 });
 $('#topScroll').click(function () {
     $("html,body").animate({ scrollTop: $('html').offset().top });
